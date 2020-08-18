985c84efdaae65b49c2bc30cce17b205d9a9ed8f
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-18 15:49:13 +0300

    make the main file exectuable

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,6 @@
+#!/usr/bin/env ruby
 require_relative '../lib/bot'
 require_relative '../lib/api_request'
 puts 'Welcome to covid-19 information bot'
 
-# api = ApiRequest.new
-# p api.is_country_exist('ethiopia')
 Bot.start_bot
