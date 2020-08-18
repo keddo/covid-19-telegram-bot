c35521f2d5133ea6814a0db91ac5d11d7543226f
keddo <adu.kedir.abdurahman@gmail.com>
2020-08-18 09:48:18 +0300

    Fix error response by the bot.

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,6 +4,7 @@ require_relative 'api_request'
 class Bot
   def self.start_bot
     Telegram::Bot::Client.run(TOKEN) do |bot|
+    begin
       bot.listen do |message|
         case message.text
         when '/start'
@@ -15,25 +16,50 @@ class Bot
           bot.api.send_message(chat_id: message.chat.id, text: ApiRequest.print_global_status(res))
         when %r{^/country}
           text = message.text.split(' ')
-          res = ApiRequest.status_by_country(text[1])
-          bot.api.send_message(chat_id: message.chat.id, text: ApiRequest.print_country_status(res), date: message.date)
+          if text[1].nil?
+            bot.api.send_message(chat_id: message.chat.id, text: "pls, enter your country like, /country ethiopia", date: message.date)
+          else 
+            res = ApiRequest.status_by_country(text[1])
+            bot.api.send_message(chat_id: message.chat.id, text: ApiRequest.print_country_status(res), date: message.date)
+          end
         when %r{^/continent}
           text = message.text.split(' ')
           continent = text.length > 2 ? (text[1] + ' ' + text[2]) : text[1]
-          res = ApiRequest.status_by_continent(continent)
-          bot.api.send_message(chat_id: message.chat.id, text: ApiRequest.print_continent_status(res), date: message.date)
+          if text.length == 1
+            bot.api.send_message(chat_id: message.chat.id, text: "pls, enter your continent like, /continent africa", date: message.date)
+          else
+            res = ApiRequest.status_by_continent(continent)
+            bot.api.send_message(chat_id: message.chat.id, text: ApiRequest.print_continent_status(res), date: message.date)
+          end 
         when %r{^/highest}
           text = message.text.split(' ')
           str = ApiRequest.top_cases(text[1].to_i)
           bot.api.send_message(chat_id: message.chat.id, text: str, date: message.date)
         when %r{^/history}
           text = message.text.split(' ')
-          str = ApiRequest.historical(text[2], text[1])
-          bot.api.send_message(chat_id: message.chat.id, text: str, date: message.date)
+          if text[1].nil? || text[2].nil?
+            bot.api.send_message(chat_id: message.chat.id, text: "make sure you give the right command", date: message.date)
+          else
+            str = ApiRequest.historical(text[2], text[1])
+            bot.api.send_message(chat_id: message.chat.id, text: str, date: message.date)
+          end
         when '/stop'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        else 
+          bot.api.send_message(chat_id: message.chat.id, text: "pls, enter the right command")
         end
       end
     end
+
+  rescue Telegram::Bot::Exceptions::ResponseError => e
+    # console.log(e)
+    p e
+    if e.error_code.to_s == '502'
+      puts 'telegram stuff, nothing to worry!'
+    elsif e.error_code.to_s == '400'
+      puts "#{e.error_code }"
+    end
+    retry
+  end
   end
 end
